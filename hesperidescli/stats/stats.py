f2cf63efb7f4459e17c1c99b9aeb6b1d2ef039b7
victor.salaun <victor.salaun@gmail.com>
2017-07-14 01:07:35 +0200

    add stats command

--- hesperidescli/stats/stats.py
+++ hesperidescli/stats/stats.py
@@ -1,6 +1,12 @@
 import click
+import json
+from hesperidescli.client import Client
 
 
 @click.command('stats')
 def command():
-    click.echo('Stats hesperides')
+    client = Client()
+    response = client.call('/rest/stats')
+    data = response.read()
+    parsed = json.loads(data)
+    print(json.dumps(parsed, indent=4, sort_keys=True))
