0e1f78f21e52686b724e5d0ffb669db5daa22c6e
victor.salaun <victor.salaun@gmail.com>
2017-07-17 22:48:25 +0200

    update events modules

--- hesperidescli/events/events.py
+++ hesperidescli/events/events.py
@@ -4,14 +4,15 @@ from hesperidescli.client import Client
 
 
 @click.command('events')
-@click.option('--stream_name')
+@click.argument('stream_name')
 @click.option('--page')
 @click.option('--size')
 def command(stream_name, page, size):
-    if not stream_name:  # if filename is not given
-        print('stream_name required')
-        return '[]'
-    params = {'page': page, 'size': size}
+    params = {}
+    if page is not None:
+        params['page'] = page
+    if size is not None:
+        params['size'] = size
     client = Client()
     response = client.get('/rest/events/' + stream_name, params)
     data = response.read()
