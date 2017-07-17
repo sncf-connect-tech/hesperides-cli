3e3c5fd6141e7f02fabaa6dfc2f4b346a1049137
victorsalaun <victor.salaun@gmail.com>
2017-07-17 11:37:21 +0200

    add feedback (hipchat) command

--- hesperidescli/feedback/feedback.py
+++ hesperidescli/feedback/feedback.py
@@ -1,6 +1,22 @@
 import click
+import json
+from hesperidescli.client import Client
 
 
-@click.command('feedback')
+@click.group('feedback')
 def command():
-    click.echo('Feedback hesperides')
+    pass
+
+
+@click.command('hipchat')
+@click.argument('message')
+def hipchat(message):
+    click.echo('Feedback Hipchat hesperides')
+    client = Client()
+    response = client.post('/rest/feedback/hipchat', message)
+    data = response.read()
+    parsed = json.loads(data)
+    print(json.dumps(parsed, indent=4, sort_keys=True))
+
+
+command.add_command(hipchat)
