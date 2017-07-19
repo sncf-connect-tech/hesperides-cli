af33595ded4f6af94f0f59fa7d4298ab73725b81
Victor SALAUN <victor.salaun@berlingotgames.com>
2017-07-19 18:19:52 +0200

    raise an exception when hesperides has not been configured

--- hesperidescli/configure/reader.py
+++ hesperidescli/configure/reader.py
@@ -4,8 +4,14 @@ from hesperidescli.configure.configfile import ConfigFile
 class ConfigFileReader(ConfigFile):
     def get_config(self):
         self.config.read(self._HOME + '/' + self._CONFIG_DIR + '/' + self._CONFIG_FILE_NAME)
-        return self.config['default']
+        try:
+            return self.config['default']
+        except KeyError:
+            raise Exception('hesperides-cli has not been configured. Please type "hesperides configure set".')
 
     def get_credentials(self):
         self.config.read(self._HOME + '/' + self._CONFIG_DIR + '/' + self._CREDENTIALS_FILE_NAME)
-        return self.config['default']
+        try:
+            return self.config['default']
+        except KeyError:
+            raise Exception('hesperides-cli has not been configured. Please type "hesperides configure set".')
