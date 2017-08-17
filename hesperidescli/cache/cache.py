652d2a996ac1ab832ce257de4420f64acfee3241
victorsalaun <victor.salaun@gmail.com>
2017-08-17 17:30:50 +0200

    add cache commands

--- hesperidescli/cache/cache.py
+++ hesperidescli/cache/cache.py
@@ -1,6 +1,145 @@
 import click
 
+from hesperidescli import utils
+from hesperidescli.client import Client
 
-@click.command('cache')
-def command():
-    click.echo('Cache hesperides')
+
+@click.command('delete-application-cache')
+@click.option('--application_name')
+@click.option('--platform_name')
+def delete_application_cache(application_name, platform_name):
+    if application_name is None:
+        print('--application_name required')
+        return ''
+    if platform_name is None:
+        print('--platform_name required')
+        return ''
+    client = Client()
+    response = client.delete('/rest/cache/application/' + application_name + '/' + platform_name)
+    utils.prettyprint(response)
+
+
+@click.command('delete-applications-cache')
+def delete_applications_cache():
+    client = Client()
+    response = client.delete('/rest/cache/applications')
+    utils.prettyprint(response)
+
+
+@click.command('delete-modules-cache')
+def delete_modules_cache():
+    client = Client()
+    response = client.delete('/rest/cache/modules')
+    utils.prettyprint(response)
+
+
+@click.command('delete-release-modules-cache')
+@click.option('--module_name')
+@click.option('--module_version')
+def delete_release_module_cache(module_name, module_version):
+    if module_name is None:
+        print('--module_name required')
+        return ''
+    if module_version is None:
+        print('--module_version required')
+        return ''
+    client = Client()
+    response = client.delete('/rest/cache/module/' + module_name + '/' + module_version + '/release')
+    utils.prettyprint(response)
+
+
+@click.command('delete-template-package-cache')
+@click.option('--template_name')
+@click.option('--template_version')
+def delete_release_template_package_cache(template_name, template_version):
+    if template_name is None:
+        print('--template_name required')
+        return ''
+    if template_version is None:
+        print('--template_version required')
+        return ''
+    client = Client()
+    response = client.delete('/rest/cache/template/package/' + template_name + '/' + template_version + '/release')
+    utils.prettyprint(response)
+
+
+@click.command('delete-templates-packages-cache')
+def delete_templates_packages_cache():
+    client = Client()
+    response = client.delete('/rest/cache/templates/packages')
+    utils.prettyprint(response)
+
+
+@click.command('delete-workingcopy-modules-cache')
+@click.option('--module_name')
+@click.option('--module_version')
+def delete_workingcopy_module_cache(module_name, module_version):
+    if module_name is None:
+        print('--module_name required')
+        return ''
+    if module_version is None:
+        print('--module_version required')
+        return ''
+    client = Client()
+    response = client.delete('/rest/cache/module/' + module_name + '/' + module_version + '/workingcopy')
+    utils.prettyprint(response)
+
+
+@click.command('delete-workingcopy-template-package-cache')
+@click.option('--template_name')
+@click.option('--module_version')
+def delete_workingcopy_template_package_cache(template_name, template_version):
+    if template_name is None:
+        print('--template_name required')
+        return ''
+    if template_version is None:
+        print('--template_version required')
+        return ''
+    client = Client()
+    response = client.delete('/rest/cache/template/package/' + template_name + '/' + template_version + '/workingcopy')
+    utils.prettyprint(response)
+
+
+@click.command('regenerate-application-cache')
+@click.option('--application_name')
+@click.option('--platform_name')
+def regenerate_application_cache(application_name, platform_name):
+    if application_name is None:
+        print('--application_name required')
+        return ''
+    if platform_name is None:
+        print('--platform_name required')
+        return ''
+    client = Client()
+    response = client.post('/rest/cache/application/' + application_name + '/' + platform_name + '/regenerate')
+    utils.prettyprint(response)
+
+
+@click.command('regenerate-module-cache')
+@click.option('--module_name')
+@click.option('--module_version')
+def regenerate_module_cache(module_name, module_version):
+    if module_name is None:
+        print('--module_name required')
+        return ''
+    if module_version is None:
+        print('--module_version required')
+        return ''
+    client = Client()
+    response = client.post('/rest/cache/module/' + module_name + '/' + module_version + '/regenerate')
+    utils.prettyprint(response)
+
+
+@click.command('regenerate-template-package-cache')
+@click.option('--template_name')
+@click.option('--template_version')
+def regenerate_template_package_cache(template_name, template_version):
+    if template_name is None:
+        print('--template_name required')
+        return ''
+    if template_version is None:
+        print('--template_version required')
+        return ''
+    client = Client()
+    response = client.post('/rest/template/package/' + template_name + '/' + template_version + '/regenerate')
+    utils.prettyprint(response)
